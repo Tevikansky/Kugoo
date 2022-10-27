857e6ff4b46e133518b7d37709a6d20558ef1841
Tevikansky <valerabogdanov@inbox.ru>
2022-10-27 23:42:36 +0600

    add: header-nav

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="ru">
+
 <head>
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -7,7 +8,10 @@
   <link rel="stylesheet" href="css/swiper-bundle.min.css">
   <link rel="stylesheet" href="css/normalize.css">
   <link rel="stylesheet" href="css/style.css">
-  <?php include_once ('./template-parts/favicon.php')?>
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Jost:wght@400;500;600;700&display=swap" rel="stylesheet">
+  <!-- <?php include_once ('./template-parts/favicon.php')?> -->
   <title>Kugo</title>
 </head>
 
@@ -15,25 +19,81 @@
 <body>
   <div class="header">
     <div class="container">
-      <ul class="header-nav-left">
-        <li class="header-nav-left-item">
-          <a href="" class="header-nav-left-link"></a>
-        </li>
-        <li class="header-nav-left-item">
-          <a href="" class="header-nav-left-link"></a>
-        </li>
-        <li class="header-nav-left-item">
-          <a href="" class="header-nav-left-link"></a>
-        </li>
-        <li class="header-nav-left-item">
-          <a href="" class="header-nav-left-link"></a>
-        </li>
-      </ul>
-      <div class="header-nav-right">
-        <a href="tel:+78005055461" class="hedear-nav-phone">+7 (800) 505-54-61</a>
+      <div class="header-nav-wrapper">
+        <ul class="header-nav-left">
+          <li class="header-nav-left-item">
+            <a href="" class="header-nav-left-link">Сервис</a>
+          </li>
+          <li class="header-nav-left-item">
+            <a href="" class="header-nav-left-link">Сотрудничество</a>
+          </li>
+          <li class="header-nav-left-item">
+            <a href="" class="header-nav-left-link">Заказать звонок</a>
+          </li>
+          <li class="header-nav-left-social">
+            <a href="" class="header-nav-left-link-social">
+              <svg width="12" height="13" class="header-nav-left-link-social-svg">
+                <use href="img/sprite.svg#Viber"></use>
+              </svg>
+            </a>
+            <a href="" class="header-nav-left-link-social">
+              <svg width="13" height="13" class="header-nav-left-link-social-svg">
+                <use href="img/sprite.svg#WhatsApp"></use>
+              </svg>
+            </a>
+            <a href="" class="header-nav-left-link-social">
+              <svg width="12" height="13" class="header-nav-left-link-social-svg">
+                <use href="img/sprite.svg#Telegram"></use>
+              </svg>
+            </a>
+          </li>
+        </ul>
+        <div class="header-nav-right">
+          <a href="tel:+78005055461" class="hedear-nav-phone">+7 (800) 505-54-61</a>
+        </div>
       </div>
     </div>
   </div>
+  <nav class="header-main">
+    <div class="container">
+      <div class="header-main-wrapper">
+        <div class="header-main-left">
+          <div class="header-main-left-logo">
+            <a href="" class="main-logo">Kugoo</a>
+          </div>
+          <div class="header-main-left-catalog">
+            <a href="" class="catalog">Каталог</a>
+          </div>
+          <div class="header-main-left-search">
+            <form action="" class="form-search"></form>
+            <button class="button-search">
+            <svg width="16" height="16" class="button-search-svg">
+                <use href="img/sprite.svg#search"></use>
+            </svg>
+            </button>
+          </div>
+        </div>
+        <div class="header-main-right">
+          <a href="#" class="header-main-right-item">
+            <svg width="20" height="20" class="header-main-right-item-svg">
+                <use href="img/sprite.svg#balance"></use>
+            </svg>
+          </a>
+          <a href="#" class="header-main-right-item">
+            <svg width="20" height="20" class="header-main-right-item-svg">
+                <use href="img/sprite.svg#favorites"></use>
+            </svg>
+          </a>
+          <a href="#" class="header-main-right-item">
+            <svg width="20" height="20" class="header-main-right-item-svg">
+                <use href="img/sprite.svg#shopping-cart"></use>
+            </svg>
+            <span class="shopping-cart-text">Корзина</span> 
+          </a>
+        </div>
+      </div>
+    </div>
+  </nav>
 </body>
 
 
