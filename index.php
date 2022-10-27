d5355398412b7f57688702e2c2316bd91ead1b51
Tevikansky <valerabogdanov@inbox.ru>
2022-10-27 21:36:53 +0600

    first commit

---
+++ index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="css/swiper-bundle.min.css">
+  <link rel="stylesheet" href="css/normalize.css">
+  <link rel="stylesheet" href="css/style.css">
+  <?php include_once ('./template-parts/favicon.php')?>
+  <title>Kugo</title>
+</head>
+
+
+<body>
+  <div class="header">
+    <div class="container">
+      <ul class="header-nav-left">
+        <li class="header-nav-left-item">
+          <a href="" class="header-nav-left-link"></a>
+        </li>
+        <li class="header-nav-left-item">
+          <a href="" class="header-nav-left-link"></a>
+        </li>
+        <li class="header-nav-left-item">
+          <a href="" class="header-nav-left-link"></a>
+        </li>
+        <li class="header-nav-left-item">
+          <a href="" class="header-nav-left-link"></a>
+        </li>
+      </ul>
+      <div class="header-nav-right">
+        <a href="tel:+78005055461" class="hedear-nav-phone">+7 (800) 505-54-61</a>
+      </div>
+    </div>
+  </div>
+</body>
+
+
+</html>
